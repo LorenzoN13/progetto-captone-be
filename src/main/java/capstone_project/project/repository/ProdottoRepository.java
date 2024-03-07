9220b6f21d8ef398d7c3f7cded1eea27e8490b5b
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-03-07 19:52:00 +0100

    creazione del ProdottoRepository

---
+++ src/main/java/capstone_project/project/repository/ProdottoRepository.java
@@ -0,0 +1,20 @@
+package capstone_project.project.repository;
+
+import capstone_project.project.model.Prodotto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProdottoRepository extends JpaRepository<Prodotto, Integer>, PagingAndSortingRepository<Prodotto, Integer> {
+    public List<Prodotto>filtraProdotti(
+            @Param("categoria") Integer categoria,
+            @Param("prezzoMinimo") Integer minPrezzo,
+            @Param("prezzoMassimo") Integer maxPrezzo,
+            @Param("prezzoSContato") Integer prezzoScontato,
+            @Param("sort") String sort);
+}
