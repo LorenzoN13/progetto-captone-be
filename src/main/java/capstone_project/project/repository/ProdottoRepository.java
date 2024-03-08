939df4015e41474a1ae842a8621f655706f11a04
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-03-08 11:58:18 +0100

    creazione delle interfacce ProdottoRepository, CategoriaRepository

--- src/main/java/capstone_project/project/repository/ProdottoRepository.java
+++ src/main/java/capstone_project/project/repository/ProdottoRepository.java
@@ -3,6 +3,7 @@ package capstone_project.project.repository;
 import capstone_project.project.model.Prodotto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,8 @@ import java.util.List;
 
 @Repository
 public interface ProdottoRepository extends JpaRepository<Prodotto, Integer>, PagingAndSortingRepository<Prodotto, Integer> {
+
+    @Query("SELECT p FROM Prodotto p" + "WHERE(P.categoria.name =: categoria OR :cagoria = '')")
     public List<Prodotto>filtraProdotti(
             @Param("categoria") Integer categoria,
             @Param("prezzoMinimo") Integer minPrezzo,
